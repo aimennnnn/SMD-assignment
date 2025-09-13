71f758c253a13e6e0a598e3404b6900c91e6bb7e
Rk_05 <aimentwo@gmail.com>
2025-09-14 00:14:39 +0500

    init repo

---
+++ app/src/main/java/com/example/myapplication/ui/explore/ExploreFragment.kt
@@ -0,0 +1,38 @@
+package com.example.myapplication.ui.explore
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.myapplication.databinding.FragmentExploreBinding
+
+class ExploreFragment : Fragment() {
+
+    private var _binding: FragmentExploreBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val homeViewModel = ViewModelProvider(this)[ExploreViewModel::class.java]
+
+        _binding = FragmentExploreBinding.inflate(inflater, container, false)  // 👈 updated
+        val root: View = binding.root
+
+        val textView: TextView = binding.textExplore
+        homeViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
