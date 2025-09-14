b7499760e596c9acb194c440dee3104c5b8d9ca5
Rk_05 <aimentwo@gmail.com>
2025-09-14 20:38:38 +0500

    Add files via upload

--- app/src/main/java/com/example/myapplication/ui/explore/ExploreFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/explore/ExploreFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.myapplication.databinding.FragmentExploreBinding
@@ -14,21 +13,18 @@ class ExploreFragment : Fragment() {
     private var _binding: FragmentExploreBinding? = null
     private val binding get() = _binding!!
 
+    private lateinit var viewModel: ExploreViewModel
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel = ViewModelProvider(this)[ExploreViewModel::class.java]
+        _binding = FragmentExploreBinding.inflate(inflater, container, false)
 
-        _binding = FragmentExploreBinding.inflate(inflater, container, false)  // 👈 updated
-        val root: View = binding.root
+        viewModel = ViewModelProvider(this)[ExploreViewModel::class.java]
 
-        val textView: TextView = binding.textExplore
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+        return binding.root
     }
 
     override fun onDestroyView() {
