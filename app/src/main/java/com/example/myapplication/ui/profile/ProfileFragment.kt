f7458a398e40b6452739999a00b0ed8ea7d2161a
Rk_05 <aimentwo@gmail.com>
2025-09-14 20:39:32 +0500

    Add files via upload

--- app/src/main/java/com/example/myapplication/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/profile/ProfileFragment.kt
@@ -1,38 +1,42 @@
 package com.example.myapplication.ui.profile
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.myapplication.databinding.FragmentProfileBinding
+import com.example.myapplication.edit_profile
+import com.example.myapplication.story_posted
 
 class ProfileFragment : Fragment() {
 
     private var _binding: FragmentProfileBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+
+    private lateinit var viewModel: ProfileViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this).get(ProfileViewModel::class.java)
-
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-        val textView: TextView = binding.textProfile
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        viewModel = ViewModelProvider(this)[ProfileViewModel::class.java]
+
+        binding.btnEditProfile.setOnClickListener {
+            startActivity(Intent(requireContext(), edit_profile::class.java))
         }
-        return root
+
+        binding.imgAvatar.setOnClickListener {
+            startActivity(Intent(requireContext(), story_posted::class.java))
+        }
+
+        return binding.root
+
     }
 
     override fun onDestroyView() {
